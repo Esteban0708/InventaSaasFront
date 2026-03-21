69abb09fda3be97873aff1b4d3c7bbe0496316ad
Esteban0708 <128402807+Esteban0708@users.noreply.github.com>
2026-03-21 01:17:16 -0500

    Nueva direccion api

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { environment } from '../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -9,7 +10,7 @@ import { jwtDecode } from 'jwt-decode';
 })
 export class AuthService {
 
-  private apiUrl = 'https://localhost:7063/api/auth';
+private apiUrl = `${environment.apiUrl}/auth`;
 
   constructor(private http: HttpClient) { }
 
